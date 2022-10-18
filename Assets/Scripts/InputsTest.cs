f399ddbb0223d1c1bc762c697574b973b15cef4e
timotee <timotee@GAULOIS-C.esagame.lan>
2022-10-18 16:53:08 +0200

    Tests for block inputs

--- Assets/Scripts/InputsTest.cs
+++ Assets/Scripts/InputsTest.cs
@@ -5,19 +5,32 @@ using UnityEngine.InputSystem;
 
 public class InputsTest : MonoBehaviour
 {
-    private Vector2 movement = Vector2.zero;
+    private Vector2 _movement = Vector2.zero;
+    private bool _block = false;
     
     public float Speed = 5;
 
     public void OnMovement(InputValue value)
     {
-        movement = value.Get<Vector2>();
+        _movement = value.Get<Vector2>();
+    }
+
+    public void OnBlock(InputValue value)
+    {
+        _block = !_block;
+
+        if (_block)
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            return;
+        }
+        GetComponent<MeshRenderer>().material.color = Color.white;
     }
     
     // Update is called once per frame
     void Update()
     {
-        Vector3 newMovement = new Vector3(movement.x, 0 , movement.y);
+        Vector3 newMovement = new Vector3(_movement.x, 0 , _movement.y);
         transform.position += newMovement * Speed * Time.deltaTime;
     }
 }
